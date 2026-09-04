240e038f218f480acec57c4b8b6e72a9fd665987
Tácito <tacito1.filho@gmail.com>
2026-09-04 17:23:38 -0300

    limpa arquivos antigos do Capture

--- StreamConfig.cs
+++
@@ -1,7 +0,0 @@
-namespace AKTelaCapture;
-
-internal sealed record StreamConfig(int Fps, int VideoBitrateMbps, bool AudioEnabled)
-{
-    public int Width => 1920;
-    public int Height => 1080;
-}
